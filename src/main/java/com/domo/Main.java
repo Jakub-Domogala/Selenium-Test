824cd6fea7853b304a14b4eec08d57661df2f616
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-08 00:31:19 +0200

    clean

--- src/main/java/com/domo/Main.java
+++ src/main/java/com/domo/Main.java
@@ -10,38 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 import pages.GoogleMainPage;
 
 public class Main {
-    static WebDriver driver;
-    static String BASE_URL = "https://www.google.com";
-    static GoogleMainPage mainPage;
-
     public static void main(String[] args) {
-        try {
-            System.out.println("Hello world!");
-
-
-            System.setProperty(
-                    "webdriver.chrome.driver",
-                    "C:\\Users\\kubad\\Desktop\\selenium1Tutorial\\selenium1\\src\\main\\resources\\chromedriver.exe"
-            );
-
-            driver = new ChromeDriver();
-            mainPage = PageFactory.initElements(driver, GoogleMainPage.class);
-            driver.get(BASE_URL);
-
-            Thread.sleep(1000);
-
-
-            Thread.sleep(1000);
-            mainPage.searchForText("cats");
-
-
-            Thread.sleep(50000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        driver.close();
-
-
+        System.out.println("Hello world!");
     }
 }
